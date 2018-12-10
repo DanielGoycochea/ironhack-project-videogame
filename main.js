1c07d6537d3c8b54c2e07a58082bed9224cab638
DanielGoycochea <danielgoycocheardz@gmail.com>
2018-12-09 21:55:45 -0600

    menu terminado

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
 $(document).ready(function(){
   console.log( "ready!" );
 })
+
+function jugar(){
+  $(".botones2").removeClass("none");
+  $(".botones").addClass("none");
+  $(".santoTraje").addClass("none")
+}
+function instrucciones(){
+  $(".botones2").addClass("none");
+  $(".instrucciones").removeClass("none");
+  $("#boton-regresar").removeClass("none");
+}
+function regresar(){
+  $(".botones2").removeClass("none");
+  $(".instrucciones").addClass("none");
+  $("#boton-regresar").addClass("none");
+  $(".botones3").addClass ("none")
+}
+function unJugador(){
+  $(".botones2").addClass("none");
+  $(".botones3").removeClass("none")
+  $("#nivel").removeClass("none");
+}
+function creditos(){
+  $(".botones2").addClass("none");
+  
+
+}
